e48372db193d731303a7a7eb4b972e8c2bfecb2e
Luke Berzins <lukeberzins16@gmail.com>
2021-02-12 16:04:34 -0500

    implementing initial routing

---
+++ client/src/components/Home.jsx
@@ -0,0 +1,6 @@
+export default function Home(props) {
+
+  return (
+    <h1>I AM Home</h1>
+  )
+}
---
+++ client/src/components/Login.jsx
@@ -0,0 +1,6 @@
+export default function Login(props) {
+
+  return (
+    <h1>I AM LOGIN</h1>
+  )
+}
