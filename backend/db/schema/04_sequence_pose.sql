a4a2f623ad8e90a5191a27a94678a026b7529aad
Luke Berzins <lukeberzins16@gmail.com>
2021-02-12 16:57:19 -0500

    changes to schema and seeds

--- backend/db/schema/04_sequence_pose.sql
+++ backend/db/schema/04_sequence_pose.sql
@@ -1,10 +1,10 @@
 -- Drop and recreate Users table (Example)
 
-DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS sequence_pose CASCADE;
 
 CREATE TABLE sequence_pose (
   id SERIAL PRIMARY KEY NOT NULL,
   sequence_id INTEGER REFERENCES sequences(id),
   pose_id INTEGER REFERENCES poses(id),
-  order INTEGER NOT NULL
+  postion INT NOT NULL
 );
