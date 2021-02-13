ea40f50a3a6e85b3562ace277b5aeb2cb2f719f8
rjlmacfarlane <rjl.macfarlane@gmail.com>
2021-02-13 17:47:06 +0000

    Add pose seeds

--- backend/db/seeds/03_pose.sql
+++ backend/db/seeds/03_pose.sql
@@ -8,13 +8,23 @@ INSERT INTO poses (name, icon, video, description, overlay)
 VALUES ('Utkatasana (Chair Pose)', '../img/icons/utkatasana.png', 'video_url', 'Excellent pose for developing balance, and for improving core strength while challenging your quads.', '../img/overlays/utkatasana.png');
 
 INSERT INTO poses (name, icon, video, description, overlay)
-VALUES ('Virabhadrasana II (Warrior 2)', '../img/icons/virabhadrasana2.png', 'video_url', 'Warrior poses are exceptionally grounding, serving to improve lower body strength by targeting the adductors, quads, and core. This pose also improves spinal alignment, deltoid strength, and overall balance.', '../img/overlays/virabhadrasana2.png');
+VALUES ('Virabhadrasana I (Warrior 1)', '../img/icons/virabhadrasana1.png', 'video_url', 'Warrior poses are exceptionally grounding, serving to improve lower body strength, spinal alignment, and overall balance.', '../img/overlays/virabhadrasana1.png');
 
 INSERT INTO poses (name, icon, video, description, overlay)
-VALUES ('Trikonasana (Triangle Pose)', '../img/icons/trikonasana.png', 'video_url', 'Trikonasana primarily stretches the hips and obliques, as well as the hamstrings. By placing your head and torso perpendicular to their normal alignment, trikonasana also doubles as an excellent balance challenge.', '../img/overlays/trikonasana.png');
+VALUES ('Virabhadrasana II (Warrior 2)', '../img/icons/virabhadrasana2.png', 'video_url', 'Warrior poses are exceptionally grounding, serving to improve lower body strength, spinal alignment, and overall balance.', '../img/overlays/virabhadrasana2.png');
+
+INSERT INTO poses (name, icon, video, description, overlay)
+VALUES ('Trikonasana (Triangle Pose)', '../img/icons/trikonasana.png', 'video_url', 'Trikonasana primarily stretches the hips and obliques. By placing your head and torso perpendicular to their normal alignment, trikonasana also doubles as an excellent balance challenge.', '../img/overlays/trikonasana.png');
 
 INSERT INTO poses (name, icon, video, description, overlay)
 VALUES ('Garudasana (Eagle Pose)', '../img/icons/garudasana.png', 'video_url', 'Garudasana (Eagle Pose), is similar to Tree Pose in that it challenges your balance on one leg. Eagle pose includes excellent stretches for the glutes, deltoids, and back muscles.', '../img/overlays/garudasana.png');
 
 INSERT INTO poses (name, icon, video, description, overlay)
-VALUES ('Half Moon Pose (Ardha Chandrasana)', '../img/icons/chandrasana.png', 'video_url', 'Garudasana (Eagle Pose), is similar to Tree Pose in that it challenges your balance on one leg. Eagle pose includes excellent stretches for the glutes, deltoids, and back muscles.', '../img/overlays/chandrasana.png');
+VALUES ('Ardha Chandrasana (Half Moon Pose)', '../img/icons/chandrasana.png', 'video_url', 'Challenge yourself with Ardha Chandrasana (Half Moon), a pose that takes your balance to its limit while stretching the hamstrings and improving hip stability.', '../img/overlays/chandrasana.png');
+
+INSERT INTO poses (name, icon, video, description, overlay)
+VALUES ('Anjaneyasana (Crescent Lunge)', '../img/icons/anjaneyasana.png', 'video_url', 'Anjaneyasana (Crescent Lunge) is simultaneously a grounding as well as uplifting pose, providing a stretch to the thoracic spine and a strength challenge for the quads (upper thigh muscles).', '../img/overlays/anjaneyasana.png');
+
+INSERT INTO poses (name, icon, video, description, overlay)
+VALUES ('Uttanasana (Forward Fold)', '../img/icons/uttanasana.png', 'video_url', 'Uttanasana (Standing Forward Fold) guides the head below the heart for a calming effect, taking pressure off the lower spine while simultaneously providing a gentle hamstring stretch.', '../img/overlays/uttanasana.png');
+
