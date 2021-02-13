3ad488fb2ebe5c94b2ec8297bdae2f6a10d2708b
rjlmacfarlane <rjl.macfarlane@gmail.com>
2021-02-13 17:22:28 +0000

    Add navbar scss rules

--- backend/db/seeds/03_pose.sql
+++ backend/db/seeds/03_pose.sql
@@ -1,4 +1,15 @@
+INSERT INTO poses (name, icon, video, description, overlay)
+VALUES ('Tadasana (Mountain Pose)', '../img/icons/tadasana.png', 'video_url', 'Tadasana (Mountain Pose): A common starting pose, tadasana improves overall posture by increasing bodily awareness. Engages abdominal and back muscles. Easily transition from tadasana to other standing poses.', '../img/overlays/tadasana.png');
 
-INSERT INTO poses (name, video, description, overlay)
-VALUES ('standing', 'video', 'desc', 1);
+INSERT INTO poses (name, icon, video, description, overlay)
+VALUES ('Vrksasana (Tree Pose)', '../img/icons/vrksasana.png', 'video_url', 'Recommended after mountain pose. Vrksasana (Tree Pose) strengthens legs, hips, glutes and obliques, and helps to improve your balance over time.', '../img/overlays/vrksasana.png');
+
+INSERT INTO poses (name, icon, video, description, overlay)
+VALUES ('Utkatasana (Chair Pose)', '../img/icons/utkatasana.png', 'video_url', 'Excellent pose for developing balance, and for improving core strength while challenging your quads.', '../img/overlays/utkatasana.png');
+
+INSERT INTO poses (name, icon, video, description, overlay)
+VALUES ('Virabhadrasana II (Warrior 2)', '../img/icons/virabhadrasana2.png', 'video_url', 'Warrior poses are exceptionally grounding, serving to improve lower body strength, spinal alignment, and overall balance.', '../img/overlays/virabhadrasana2.png');
+
+INSERT INTO poses (name, icon, video, description, overlay)
+VALUES ('Trikonasana (Triangle Pose)', '../img/icons/trikonasana.png', 'video_url', 'Trikonasana primarily stretches the hips and obliques. By placing your head and torso perpendicular to their normal alignment, trikonasana also doubles as an excellent balance challenge.', '../img/overlays/trikonasana.png');
 
