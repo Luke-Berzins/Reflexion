afb6a91f39ff2dacba07ccf3d039b9fc2cc4591d
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-02-13 15:44:18 +0000

    added nav

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,24 +18,23 @@ function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-          <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/login">Login</Link>
-            </li>
-            <li>
-              <Link to="/register">Register</Link>
-            </li>
-            <li>
-              <Link to="/builder">Builder</Link>
-            </li>
-            <li>
-              <Link to="/session">Session</Link>
-            </li>
-          </ul>
+        <nav className="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+          <div className="container">
+            <a className="navbar-brand js-scroll-trigger" href="#page-top">Reflexion</a>
+            <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+              Menu
+              <i className="fas fa-bars"></i>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarResponsive">
+              <ul className="navbar-nav ml-auto">
+                <li className="nav-item"><Link className="nav-link js-scroll-trigger" to="/">Home</Link></li>
+                <li className="nav-item"><Link className="nav-link js-scroll-trigger" to="/login">Login</Link></li>
+                <li className="nav-item"><Link className="nav-link js-scroll-trigger" to="/register">Register</Link></li>
+                <li className="nav-item"><Link className="nav-link js-scroll-trigger" to="/builder">Builder</Link></li>
+                <li className="nav-item"><Link className="nav-link js-scroll-trigger" to="/session">Session</Link></li>
+              </ul>
+            </div>
+          </div>
         </nav>
         <Switch>
           <Route path="/builder">
