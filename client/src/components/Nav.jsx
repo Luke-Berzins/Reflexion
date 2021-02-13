5673f5da0215886111cabf9d4ab972294f4f9a58
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-02-13 18:06:27 +0000

    added some css to nav

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -8,21 +8,20 @@ import {
 export default function Nav(props) {
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+    <nav className="navbar fixed-top navbar-expand-lg navbar-light fixed-top" id="mainNav">
           <div className="container">
-            <a className="navbar-brand js-scroll-trigger" href="#page-top">Reflexion</a>
+            <Link className="navbar-brand js-scroll-trigger" to="/">Reflexion</Link>
             <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
               Menu
               <i className="fas fa-bars"></i>
             </button>
             <div className="collapse navbar-collapse" id="navbarResponsive">
               <ul className="navbar-nav ml-auto">
-                <li className="nav-item"><Link className="nav-link js-scroll-trigger" to="/">Home</Link></li>
-                <li className="nav-item"><Link className="nav-link js-scroll-trigger" to="/login">Login</Link></li>
-                <li className="nav-item"><Link className="nav-link js-scroll-trigger" to="/register">Register</Link></li>
                 <li className="nav-item"><Link className="nav-link js-scroll-trigger" to="/builder">Builder</Link></li>
                 <li className="nav-item"><Link className="nav-link js-scroll-trigger" to="/session">Session</Link></li>
                 <li className="nav-item"><Link className="nav-link js-scroll-trigger" to="/about">About</Link></li>
+                <li className="nav-item"><Link className="nav-link js-scroll-trigger" to="/login">Login</Link></li>
+                <li className="nav-item"><Link className="nav-link js-scroll-trigger" to="/register">Register</Link></li>
               </ul>
             </div>
           </div>
--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -1,6 +1,8 @@
 export default function Register(props) {
 
   return (
+    <div>
     <h1>I AM REGISTER</h1>
+    </div>
   )
 }
