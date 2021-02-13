669c68a8eab3578392370446812b2669dd836a1d
rjlmacfarlane <rjl.macfarlane@gmail.com>
2021-02-13 23:32:22 +0000

    Merge styles > register/about

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -6,9 +6,6 @@ export default function About(props) {
     <div id='about'>
 
       <div className="container">
-        <h1 className="mt-4 mb-3">About
-          <small> Reflexion</small>
-        </h1>
 
         <div className="row">
           <div className="col-lg-6">
@@ -16,22 +13,23 @@ export default function About(props) {
           </div>
           <div className="col-lg-6">
             <h2>About Reflexion</h2>
-            <p>Explore yoga history with this collection of insightful articles from Sadhguru spanning from Adiyogi and the origin of yoga over 15,000 years ago to present day. Gain a deeper understanding of yoga and learn about various masters, gurus, sages and saints throughout the ages.</p>
+            <p>Be your own teacher. Reflexion is designed to gently guide you into a home yoga practice. Achieve your physical and spiritual goals as you discover your individual practice with our unique, interactive, instructor-designed interface. </p>
+            <p>This is a conceptual project created as a final project for the Lighthouse Labs web development bootcamp.</p>
           </div>
         </div>
 
-        <h2>Our Team</h2>
+        <h2>Reflexion Developers</h2>
 
         <div className="row">
           <div className="col-lg-4 mb-4">
             <div className="card h-100 text-center">
               <img className="card-img-top" src="https://ca.slack-edge.com/T2G8TE2E5-U01CYFLFL3U-8bde8aadf2d0-512" alt="Mahsa pic" width="750" height="350" />
               <div className="card-body">
-                <h4 className="card-title">Team Member</h4>
-                <h6 className="card-subtitle mb-2 text-muted">Manager</h6>
+                <h4 className="card-title">Mahsa Arabameri</h4>
+                <h6 className="card-subtitle mb-2 text-muted">Developer</h6>
               </div>
               <div className="card-footer">
-                <a href="#">mahsa@example.com</a>
+                <a href="https://github.com/Mahsa1990a">Mahsa's Github</a>
               </div>
             </div>
           </div>
@@ -39,11 +37,11 @@ export default function About(props) {
             <div className="card h-100 text-center">
               <img className="card-img-top" src="https://ca.slack-edge.com/T2G8TE2E5-U01EXRDMKQT-cb38ed1f2dc8-512" alt="Ryan pic" width="750" height="350" />
               <div className="card-body">
-                <h4 className="card-title">Team Member</h4>
-                <h6 className="card-subtitle mb-2 text-muted">Manager</h6>
+                <h4 className="card-title">Ryan MacFarlane</h4>
+                <h6 className="card-subtitle mb-2 text-muted">Developer/Yoga Instructor</h6>
               </div>
               <div className="card-footer">
-                <a href="#">ryan@example.com</a>
+                <a href="https://github.com/rjlmacfarlane">Ryan's GitHub</a>
               </div>
             </div>
           </div>
@@ -51,35 +49,13 @@ export default function About(props) {
             <div className="card h-100 text-center">
               <img className="card-img-top" src="https://ca.slack-edge.com/T2G8TE2E5-U01F3Q30MHA-8e4d113aeb3c-512" alt="Luke pic" width="500" height="350" />
               <div className="card-body">
-                <h4 className="card-title">Team Member</h4>
-                <h6 className="card-subtitle mb-2 text-muted">Manager</h6>
+                <h4 className="card-title">Luke Berzins</h4>
+                <h6 className="card-subtitle mb-2 text-muted">Developer</h6>
               </div>
               <div className="card-footer">
-                <a href="#">luke@example.com</a>
+                <a href="https://github.com/Luke-Berzins">Luke's GitHub</a>
               </div>
             </div>
-          </div>
-        </div>
-
-        <h2>Our Customers</h2>
-        <div className="row">
-          <div className="col-lg-2 col-sm-4 mb-4">
-            <img className="img-fluid" src="https://assets.teenvogue.com/photos/5b4e34b4942af253ca1a7a54/master/pass/16-pikachu-with-bangs.nocrop.w710.h2147483647.jpg" alt="" width="500" height="300" />
-          </div>
-          <div className="col-lg-2 col-sm-4 mb-4">
-            <img className="img-fluid" src="https://assets.teenvogue.com/photos/5b4e34b4942af253ca1a7a54/master/pass/16-pikachu-with-bangs.nocrop.w710.h2147483647.jpg" alt=""  width="500" height="300" />
-          </div>
-          <div className="col-lg-2 col-sm-4 mb-4">
-            <img className="img-fluid" src="https://assets.teenvogue.com/photos/5b4e34b4942af253ca1a7a54/master/pass/16-pikachu-with-bangs.nocrop.w710.h2147483647.jpg" alt="" width="500" height="300" />
-          </div>
-          <div className="col-lg-2 col-sm-4 mb-4">
-            <img className="img-fluid" src="https://assets.teenvogue.com/photos/5b4e34b4942af253ca1a7a54/master/pass/16-pikachu-with-bangs.nocrop.w710.h2147483647.jpg" alt="" width="500" height="300" />
-          </div>
-          <div className="col-lg-2 col-sm-4 mb-4">
-            <img className="img-fluid" src="https://assets.teenvogue.com/photos/5b4e34b4942af253ca1a7a54/master/pass/16-pikachu-with-bangs.nocrop.w710.h2147483647.jpg" alt="" width="500" height="300" />
-          </div>
-          <div className="col-lg-2 col-sm-4 mb-4">
-            <img className="img-fluid" src="https://assets.teenvogue.com/photos/5b4e34b4942af253ca1a7a54/master/pass/16-pikachu-with-bangs.nocrop.w710.h2147483647.jpg" alt="" width="500" height="300" />
           </div>
         </div>
       </div>
