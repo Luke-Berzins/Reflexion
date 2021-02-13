27bffb58b7aff0a8a7410749fe8b7f51a94c0137
rjlmacfarlane <rjl.macfarlane@gmail.com>
2021-02-13 19:49:00 +0000

    Begin webcam logic

--- client/src/components/Session.jsx
+++ client/src/components/Session.jsx
@@ -1,6 +1,18 @@
+// import ReactPlayer from "react-player";
+
 export default function Session(props) {
 
   return (
-    <h1>I AM Session</h1>
-  )
+    <div id="container">
+
+      <video id="video" width="1024" height="768"></video>
+      <script src="js/video.js" type="text/javascript"></script>
+      {/* <div className="video-embed">
+
+        <ReactPlayer
+          url="https://www.youtube.com/watch?v=QH2-TGUlwu4"
+        />
+      </div> */}
+    </div>
+  );
 }
