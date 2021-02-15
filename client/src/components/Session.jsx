1f6e68e0013d7e3f63daa39b1adda584a1475668
rjlmacfarlane <rjl.macfarlane@gmail.com>
2021-02-15 16:22:30 +0000

    Add logo to navbar

--- client/src/components/Session.jsx
+++ client/src/components/Session.jsx
@@ -4,7 +4,7 @@ import Webcam from "react-webcam"
 
 export default function Session(props) {
 
-  const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+  const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0) + 7;
   const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0) - 7;
 
   const videoConstraints = {
