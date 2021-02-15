666b441674d01e2833df5724971be57e97ff8129
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-02-15 21:59:20 +0000

    register

--- client/src/components/Session.jsx
+++ client/src/components/Session.jsx
@@ -2,29 +2,31 @@
 import React from "react";
 import Webcam from "react-webcam"
 
+import "./Session.scss"
+
 export default function Session(props) {
 
   const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
   const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0) - 7;
 
   const videoConstraints = {
-    width: { min: vw },
-    height: { min: vh },
+    // width: { min: vw },
+    // height: { min: vh },
+    facingMode: "user"
   };
 
 
   return (
     <div id="video-container">
 
-      <Webcam
-        audio={ false }
-        width={ vw }
-        height={ vh }
-        videoConstraints={ videoConstraints }
-      />
+        <Webcam
+          audio={ false }
+          width={ vw }
+          height={ vh }
+          videoConstraints={ videoConstraints }
+        />
 
       {/* <div className="video-embed">
-
         <ReactPlayer
           url="https://www.youtube.com/watch?v=QH2-TGUlwu4"
         />
