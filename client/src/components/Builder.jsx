36adfa0a4c27a2ea381bba05daf67da3aeb76ae9
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-02-16 00:56:53 +0000

    added drag & drop

--- client/src/components/Builder.jsx
+++ client/src/components/Builder.jsx
@@ -1,10 +1,25 @@
+import React from 'react'
+import Board from './Board'
+import Card from './Card'
+
+import "./Builder.scss"
+
 export default function Builder(props) {
 
   return (
     <div>
-      <h1>I AM Builder</h1>
-
-
+      <main className='flexbox'>
+        <Board id="board_1" className="board">
+          <Card id='card_1' className="card" draggable="true">
+            <p>Card One</p>
+          </Card>
+        </Board>
+        <Board id="board_2" className="board">
+          <Card id='card_2' className="card" draggable="true">
+            <p>Card Two</p>
+          </Card>
+        </Board>
+      </main>
     </div>
   )
 }
