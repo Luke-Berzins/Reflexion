6a2554ac128ac9a975a3e169b4bee403964b8c0a
rjlmacfarlane <rjl.macfarlane@gmail.com>
2021-02-16 18:23:26 +0000

    Add transition effects to sessions page

--- client/src/components/Session.jsx
+++ client/src/components/Session.jsx
@@ -14,32 +14,33 @@ export default function Session(props) {
 
   return (
     <div>
-      <div className="overlay">
-        <img src="https://i.imgur.com/ApU8PwB.png" alt="overlay" style={{opacity: 0.75}} />
-
-
-      </div>
-
-      <div id="video-embed" className="video-embed">
+      <div id="video-embed" className="video-embed animate__animated animate__fadeIn animate__slower animate__delay-2s">
         <ReactPlayer
-          playing="true"
+          playing
           url="https://www.youtube.com/watch?v=Fr5kiIygm0c"
         />
       </div>
 
-    <div id="video-container" className="animate__animated animate__fadeIn animate__slower">
+    <div className="pose-cam-container animate__animated animate__fadeIn animate__slower animate__delay-0.5s">
 
-      <center>
-      <Webcam
-        style = { { width: 'auto', height: '99vh', } }
-        mirrored         = { true }
-        audio            = { false }
-        width            = { 1280 }
-        height           = { 720 }
-        videoConstraints = { videoConstraints }
-      />
-      </center>
+      <div className="overlay">
+        <img src="https://i.imgur.com/ApU8PwB.png" alt="overlay" style={{opacity: 0.75}} />
+      </div>
 
+      <div id="video-container">
+
+        <center>
+        <Webcam
+          style = { { width: 'auto', height: '99vh', } }
+          mirrored         = { true }
+          audio            = { false }
+          width            = { 1280 }
+          height           = { 720 }
+          videoConstraints = { videoConstraints }
+        />
+        </center>
+
+        </div>
       </div>
     </div>
 
