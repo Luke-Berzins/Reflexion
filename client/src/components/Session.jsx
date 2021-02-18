13870fe2cf4db7a240ebbcec2d314e0f986a6ee7
rjlmacfarlane <rjl.macfarlane@gmail.com>
2021-02-18 23:54:48 +0000

    Voice detection overhaul

--- client/src/components/Session.jsx
+++ client/src/components/Session.jsx
@@ -9,26 +9,32 @@ import { useState, useEffect } from "react";
 
 
 
+let counter = 0
 export default function Session(props) {
 
   const [state, setState] = useState({
     poseArray : [],
-    currentPose: {}
+    currentPose: {},
+    pageView: false
   })
   let { id } = useParams();
   let poseArray = [];
-  const setList = id => {
-    axios.get("/api/poses/build", {
-      params: {
-        session: id
-      }
-    })
-    .then(res => {
-      console.log("RES", res.data)
-      poseArray = res.data;
-    }).catch(error => {
-      console.log(error)
-    })
+
+  const startSequence = () => {
+    setState(prev => ({...prev, pageView: true}))
+  }
+
+  const poseIncrementer = (direction) => {
+
+    if (direction === 100) {
+      if (counter >= state.poseArray.length - 1) { return; }
+      counter++
+      setState(prev => ({...prev, currentPose: state.poseArray[counter]}));
+    } else if (direction === 200) {
+      if (counter === 0) { return; }
+      counter--
+      setState(prev => ({...prev, currentPose: state.poseArray[counter]}));
+    }
   }
 
   useEffect(() => {
@@ -43,55 +49,61 @@ export default function Session(props) {
     })
   }, []);
 
-
-
-
   const videoConstraints = {
     facingMode: "user",
     width:  { min: 1280 },
     height: { min: 720 },
   };
 
-  return (
+  if (!state.pageView) {
 
-    <div id="session" onload="setList(id)" className="animate__animated animate__fadeIn">
+    return (
 
-      <div className="voice-detection">
-       <VoiceDetection />
+      <div id="session" onload="setList(id)" className="animate__animated animate__fadeIn">
+        <div className="voice-detection">
+         <VoiceDetection
+         startSequence={ startSequence } />
+        </div>
       </div>
 
-      <div id="video-embed" className="video-embed animate__animated animate__fadeIn animate__slower animate__delay-3s">
+    )
 
-        <ReactPlayer
-          playing
-          url={state.currentPose.video}
-        />
+  } else {
 
-      </div>
-    <div className="pose-cam-container animate__animated animate__fadeIn animate__slower animate__delay-2s">
-      <div className="overlay">
-        <img src={state.currentPose.overlay} alt="overlay" style={{opacity: 0.75}} />
-        {/* <img src='https://i.imgur.com/xjorBOU.png' alt="overlay" style={{opacity: 0.75}} /> */}
-      </div>
-        <div id="video-container">
+    return (
 
-          <center>
-          <Webcam
-            style = { { width: 'auto', height: '99vh', } }
-            mirrored         = { true }
-            audio            = { false }
-            width            = { 1280 }
-            height           = { 720 }
-            videoConstraints = { videoConstraints }
+      <div id="session" onload="setList(id)" className="animate__animated animate__fadeIn">
+        <div className="voice-detection">
+         <VoiceDetection
+         poseIncrementer={ poseIncrementer }/>
+       </div>
+
+        <div id="video-embed" className="video-embed animate__animated animate__fadeIn animate__slower animate__delay-3s">
+          <ReactPlayer
+            playing
+            url={state.currentPose.video}
           />
-          </center>
-
         </div>
-        <div>
-          <h1> !! {state.currentPose.video} !!</h1>
 
+      <div className="pose-cam-container animate__animated animate__fadeIn animate__slower animate__delay-2s">
+        <div className="overlay">
+          <img src={state.currentPose.overlay} alt="overlay" style={{opacity: 0.75}} />
+        </div>
+
+          <div id="video-container">
+            <center>
+            <Webcam
+              style = { { width: 'auto', height: '99vh', } }
+              mirrored         = { true }
+              audio            = { false }
+              width            = { 1280 }
+              height           = { 720 }
+              videoConstraints = { videoConstraints }
+            />
+            </center>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
