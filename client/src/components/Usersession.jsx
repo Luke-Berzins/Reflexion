8656cb5a9736891b006e5b48c145c28065c792d8
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-02-19 20:33:17 +0000

    more css

--- client/src/components/Usersession.jsx
+++ client/src/components/Usersession.jsx
@@ -5,9 +5,8 @@ export default function Usersession(props) {
   return (
     <div className='usersession'>
 
-      <h1>Your Sessions:</h1>
+      <h1>{props.name}</h1>
       <div>
-        <h1>{props.name}</h1>
         <button className="btn btn-info" onClick={() => window.locatio= `/session/${props.id}`}>Start</button>
         <button className="btn btn-danger" type='submit'>Delete</button>
       </div>
