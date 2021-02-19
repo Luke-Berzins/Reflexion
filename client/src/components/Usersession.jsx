badd2718cbcdfa2f0ec12f423bd3d9da21def938
Luke Berzins <lukeberzins16@gmail.com>
2021-02-19 15:29:39 -0500

    merging

--- client/src/components/Usersession.jsx
+++ client/src/components/Usersession.jsx
@@ -4,9 +4,6 @@ import {useState, useEffect} from "react"
 export default function Usersession(props) {
   return (
     <div>
-
-      <br></br>
-      <br></br>
       <h1>{props.name}</h1>
       <button onClick={() => window.locatio= `/session/${props.id}`}>Start</button>
     </div>
